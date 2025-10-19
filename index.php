211185e31a14dbffc68479d2239e7decbfb0a25a
Acu1000 <acu1000.developer@gmail.com>
2025-10-19 20:42:55 +0200

    Improved routing

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
-require_once 'Routing.php';
+require_once 'Router.php';
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
-Routing::run($path);
+$router = new Router();
+
+$router->get('/login', function() {
+    include 'public/views/login.html';
+});
+
+$router->get('/dashboard', function() {
+    include 'public/views/dashboard.html';
+});
+
+$router->dispatch();
